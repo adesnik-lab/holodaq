45ae6e26a1c7e6c84c0b8b76a207ccc7002548b0
kevinksit <kevinkamansit@gmail.com>
2023-06-14 15:13:32 -0700

    updating extract to be recursive

--- modules/abs/Module.m
+++ modules/abs/Module.m
@@ -27,7 +27,23 @@ classdef Module < dynamicprops
         %     obj.submodules.add(submodule);
         % end
         
+        function out = get_objs(obj)
+            props = properties(obj);
+            is_obj = cellfun(@(x) isobject(x), props);
+            if ~any(is_obj)
+                return;
+            end
+            out = cat(2,  obj.get_objs, obj.(props{is_obj})); % will this work?
+        end
+
         function out = extract(obj, query)
+            all_objs = obj.get_objs();
+            all_objs(~isa(all_objs, query)) = [];
+            out = all_objs;
+        end
+
+        function out = extract2(obj, query)
+            % this needs to be recursive?
             props = properties(obj);
             is_queried = cellfun(@(x) isa(obj.(x), query), props);
             if ~any(is_queried)
