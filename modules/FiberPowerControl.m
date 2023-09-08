0b1b6461f886f132fc523185098bba4acbcac875
kevinksit <kevinkamansit@gmail.com>
2023-09-08 14:11:37 -0700

    New fiber tester app

--- modules/FiberPowerControl.m
+++ modules/FiberPowerControl.m
@@ -7,9 +7,14 @@ classdef FiberPowerControl < Module
 
     methods
         function obj = FiberPowerControl(shutter, hwp, path_to_lut)
+            if nargin < 3 || isempty(path_to_lut)
+                obj.hwp_lut = [];
+            else
+                obj.hwp_lut = importdata(path_to_lut);
+            end
+
             obj.shutter = shutter;
             obj.hwp = hwp;
-            obj.hwp_lut = importdata(path_to_lut); % required
         end
 
         function set_power(obj, pwr)
@@ -18,6 +23,13 @@ classdef FiberPowerControl < Module
         
         function prepare(obj)
             obj.hwp.moveto(pwr2deg(obj.pwr));
+        end
+
+        function deg = pwr2deg(obj, pwr)
+            if isempty(obj.hwp_lut)
+                error('No LUT provided, cannot convert.')
+            end
+            deg = obj.hwp_lut(pwr);
         end
     end
 end
