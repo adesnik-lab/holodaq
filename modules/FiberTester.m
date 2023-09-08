c39e8b6027b2a44db14368add55f20c65a4ff0f0
Kevin SIt <33428056+neurognome@users.noreply.github.com>
2023-09-08 12:43:32 -0700

    meh

--- modules/FiberTester.m
+++ modules/FiberTester.m
@@ -3,13 +3,15 @@ classdef FiberTester < Module
         laser_920
         laser_1100
         si_trigger
+        clock
     end
     
     methods
-        function obj = FiberTester(laser_920, laser_1100, si_trigger)
+        function obj = FiberTester(laser_920, laser_1100, si_trigger, clock)
             obj.laser_920 = laser_920;
             obj.laser_1100 = laser_1100;
             obj.si_trigger = si_trigger;
+            obj.clock = clock;
         end
     end
 end
