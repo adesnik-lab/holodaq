d25b88c605334b81243b46454643f8cbd9c93706
Kevin SIt <33428056+neurognome@users.noreply.github.com>
2026-07-10 10:58:32 -0700

    Update LaserGate.m

--- modules/LaserGate.m
+++ modules/LaserGate.m
@@ -1,10 +1,49 @@
+% classdef LaserGate < Module
+%     properties
+%         gate
+%     end
+% 
+%     properties (Constant)
+%         output_value = 3.5; % v, measured for max, maybe we should remeasure this sometime?
+%     end
+% 
+%     methods
+%         function obj = LaserGate(gate)
+%             obj.gate = gate;
+%         end
+% 
+%         function set(obj, s, trial_duration)
+%             fs = obj.gate.interface.sample_rate;
+%             sweep = zeros(trial_duration*fs, 1);
+%             n_pulses = length(s.pulse_start);
+%             for i = 1:n_pulses
+%                 sweep(round(s.pulse_start(i)*fs) : round((s.pulse_start(i) + s.pulse_duration(i))*fs)) = obj.output_value;
+%             end
+%             obj.gate.set(sweep); 
+%         end
+% 
+%         function set_old(obj, stim, trial_duration)  
+%             fs = obj.gate.interface.sample_rate;
+%             sweep = zeros(trial_duration*fs, 1);
+%             for s = stim
+%                 % loop through the starts and durations and put em in
+%                 n_stims = length(s.pulse_start);
+%                 for i = 1:n_stims
+%                     sweep(round(s.pulse_start(i)*fs) : round((s.pulse_start(i) + s.pulse_duration(i))*fs)) = obj.output_value;
+%                 end
+%             end
+%             obj.gate.set(sweep);
+%         end
+%     end
+% end
+
 classdef LaserGate < Module
     properties
         gate
     end
 
     properties (Constant)
-        output_value = 3.5; % v, measured for max, maybe we should remeasure this sometime?
+        output_value = 3.5; % v, measured for max
     end
 
     methods
@@ -12,17 +51,7 @@ classdef LaserGate < Module
             obj.gate = gate;
         end
 
-        function set(obj, s, trial_duration)
-            fs = obj.gate.interface.sample_rate;
-            sweep = zeros(trial_duration*fs, 1);
-            n_pulses = length(s.pulse_start);
-            for i = 1:n_pulses
-                sweep(round(s.pulse_start(i)*fs) : round((s.pulse_start(i) + s.pulse_duration(i))*fs)) = obj.output_value;
-            end
-            obj.gate.set(sweep); 
-        end
-
-        function set_old(obj, stim, trial_duration)  
+        function set(obj, stim, trial_duration)  
             fs = obj.gate.interface.sample_rate;
             sweep = zeros(trial_duration*fs, 1);
             for s = stim
@@ -36,4 +65,5 @@ classdef LaserGate < Module
         end
     end
 end
+
 
